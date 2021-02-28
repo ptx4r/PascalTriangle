933a61ab52fa8ca7dd89a87b11cb9f2d77d056f9
ptx4r <79636485+ptx4r@users.noreply.github.com>
2021-02-28 16:28:00 +0200

    Add files via upload

---
+++ PascalTriangle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Integer[] pascal = new Integer[n + 1];
+        Integer[] pascalOld = new Integer[n + 1];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            pascal[i + 1] = 0;
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    System.out.print("1");
+                    pascal[j] = 1;
+                    continue;
+                }
+                sum = pascalOld[j] + pascalOld[j - 1];
+                pascal[j] = sum;
+                System.out.print(sum+" ");
+            }
+            for (int j = 0; j < pascal.length; j++) {
+                if (pascal[j] != null) {
+                    pascalOld[j] = pascal[j];
+
+                }
+            }
+            System.out.println();
+        }
+    }
+}
